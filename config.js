232893531afdee1dc9b799a445267c72790aba01
amanuwa <amanhamruha@gmail.com>
2026-09-18 02:15:01 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // 🔒 CORE INFRASTRUCTURE CONFIGURATION (DO NOT SHARE)
 // ==========================================
 
-export const SUPABASE_URL = 'https://cqkaxhyrnjmqpovxwjom.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxa2F4aHlybmptcXBvdnh3am9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMzE3NDQsImV4cCI6MjA1Njc5MTc0NH0.eTI1DrOdV3SK_d9yEAYzN6X-1C6EKKNnoavQxGLU6-o';
+export const SUPABASE_URL='https://cqkaxhyrnjmqpovxwjom.supabase.co';
+export const SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxa2F4aHlybmptcXBvdnh3am9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMTkwMTcsImV4cCI6MjA5ODY5NTAxN30.eT1lDRDoV3SK_d9yEAYzN6X-lC6EKkNNoavQxgLU6-Q';
 
 // Initialize Supabase Client
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
